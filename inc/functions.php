94ba65fb735b305577fc16d70f95bde9e7d5fc1f
Angrej Kumar <angrej.kumar@gmail.com>
2026-08-13 10:09:51 +0530

    Update version to 1.0.47 and refine content cleaning logic in get_editor_output function to enhance output formatting.

--- inc/functions.php
+++ inc/functions.php
@@ -387,7 +387,7 @@ function get_editor_output($content) {
   $content = trim($content);
   $content = preg_replace('/^(<br>)+|(<br>)+$/', '', $content);
   $content = str_replace('<p><br></p>', '', $content);
-  $content = str_replace('<div><br></div>', '', $content);
+  $content = preg_replace('/(?:<div><br><\/div>\s*)+$/', '', $content);
   $content = preg_replace('/<div>\s*<\/div>/i', '', $content);
   return $content;
 }
